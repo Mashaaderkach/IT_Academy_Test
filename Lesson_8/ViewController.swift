f091147d9149aa066ab574538866975fada0416c
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-04-11 01:09:07 +0300

    XX-4. Print added

--- Lesson_8/ViewController.swift
+++ Lesson_8/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         labelOne.text = "I'm here"
+        
+        print("And what about you?")
     }
 
 
